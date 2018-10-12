94b9322a3ea2ed5a27d267cfd306583c699629b2
Cleyton Gonçalves <cleyton@cleytongoncalves.com>
2018-10-12 18:28:49 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-app>
+
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+          @click="selectedItem = item.icon"
+        >
+
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+
+        </v-list-tile>
+      </v-list>
+
+    </v-navigation-drawer>
+
+    <v-toolbar
+      app
+      :clipped-left="clipped"
+    >
+
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+
+      <v-toolbar-title>{{ title }} </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-toolbar>
+
+    <v-content>
+      <pacientes/>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018 - FATA - IC - UFMT</span>
+    </v-footer>
+
+  </v-app>
+</template>
+
+<script>
+import Pacientes from "./components/Pacientes";
+
+export default {
+  name: "App",
+  components: {
+    Pacientes
+  },
+  data() {
+    return {
+      clipped: true,
+      drawer: true,
+      fixed: false,
+      items: [
+        {
+          title: "Pacientes",
+          icon: "sentiment_satisfied_alt",
+          isSelected: true,
+        },
+        {
+          title: "Sujeitos",
+          icon: "accessibility_new"
+        },
+        {
+          title: "Verbos",
+          icon: "pool"
+        },
+        {
+          title: "Complementos",
+          icon: "local_florist"
+        },
+        {
+          title: "Diversos",
+          icon: "format_quote"
+        }
+      ],
+      miniVariant: false,
+      title: "Minha Voz"
+    };
+  }
+};
+</script>
